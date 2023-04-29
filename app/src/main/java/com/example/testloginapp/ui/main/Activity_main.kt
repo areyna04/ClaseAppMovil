0df1bafba97b8491b586e7ce19a682b8eb477c47
lwilches <lwilches@segurosmundal.com.co>
2023-04-29 00:13:09 -0500

    Add: Se agregan las vistas

---
+++ app/src/main/java/com/example/testloginapp/ui/main/Activity_main.kt
@@ -0,0 +1,12 @@
+package com.example.testloginapp.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testloginapp.R
+
+class activity_main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

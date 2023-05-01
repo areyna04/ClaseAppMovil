073ec6778d66a1be7f0bfc24654e664b282a7360
Arturo Reyna <a.reyna@uniandes.edu.co>
2023-04-30 20:47:38 -0600

    fixes en gradle

--- app/src/test/java/com/example/testloginapp/ListAlbumUnitTest.kt
+++ app/src/test/java/com/example/testloginapp/ListAlbumUnitTest.kt
@@ -1,19 +1,25 @@
 package com.example.testloginapp
 
 import android.app.Application
+import com.android.volley.VolleyError
 import org.junit.Test
 import org.junit.Assert.*
 import com.example.testloginapp.data.model.Album
 import com.example.testloginapp.repositories.AlbumRepository
-
+import com.example.testloginapp.network.NetworkServiceAdapter
+import com.android.volley.toolbox.*
+import org.json.JSONArray
+import org.mockito.Mockito.*
 
 class ListAlbumTest {
     @Test
     fun getAlbumsReturnsListOfAlbums() {
-        val application = Application()
-        val albums_remote = mutableListOf<Album>()
-        val albumRepository = AlbumRepository(application)
+        val mockApplication = mock(Application::class.java)
+        val albumRepository = AlbumRepository(mockApplication)
+        val emptyResult: Unit? = null
+        val mockCallback: (List<Album>) = emptyList<Album>()
+        val mockErrorCallback: (VolleyError) -> Unit = mock()
 
-        assertEquals(4, 2 + 2)
+        albumRepository.refreshData((mockCallback) -> Unit, mockErrorCallback)
     }
 }

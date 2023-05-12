f00636140f9bec27edfde00edad418f5fe93de36
Arturo Reyna <a.reyna@uniandes.edu.co>
2023-05-12 01:26:00 -0600

    Agregados test funcionales todas las funcionalidades

---
+++ app/src/androidTest/java/com/example/testloginapp/ui/login/ConsultarCatalogoAlbumsTest.kt
@@ -0,0 +1,81 @@
+package com.example.testloginapp.ui.login
+
+
+import androidx.test.espresso.DataInteraction
+import androidx.test.espresso.ViewInteraction
+import androidx.test.filters.LargeTest
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewParent
+
+import androidx.test.InstrumentationRegistry.getInstrumentation
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+
+import com.example.testloginapp.R
+
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.anything
+import org.hamcrest.Matchers.`is`
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class ConsultarCatalogoAlbumsTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(LoginActivity::class.java)
+
+    @Test
+    fun consultarCatalogoAlbumsTest() {
+        val materialButton = onView(
+allOf(withId(R.id.BtnUsuario), withText("Usuario"),
+childAtPosition(
+allOf(withId(R.id.container),
+childAtPosition(
+withId(android.R.id.content),
+0)),
+2),
+isDisplayed()))
+        materialButton.perform(click())
+        
+        val textView = onView(
+allOf(withText("Albums"),
+withParent(allOf(withId(R.id.my_toolbar),
+withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java)))),
+isDisplayed()))
+        textView.check(matches(withText("Albums")))
+        }
+    
+    private fun childAtPosition(
+            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+    }
---
+++ app/src/androidTest/java/com/example/testloginapp/ui/login/CreateAlbumTest.kt
@@ -0,0 +1,206 @@
+package com.example.testloginapp.ui.login
+
+
+import androidx.test.espresso.DataInteraction
+import androidx.test.espresso.ViewInteraction
+import androidx.test.filters.LargeTest
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewParent
+
+import androidx.test.InstrumentationRegistry.getInstrumentation
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+
+import com.example.testloginapp.R
+
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.anything
+import org.hamcrest.Matchers.`is`
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class CreateAlbumTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(LoginActivity::class.java)
+
+    @Test
+    fun createAlbumTest() {
+        val materialButton = onView(
+allOf(withId(R.id.BtnColeccionista), withText("Coleccionista"),
+childAtPosition(
+allOf(withId(R.id.container),
+childAtPosition(
+withId(android.R.id.content),
+0)),
+1),
+isDisplayed()))
+        materialButton.perform(click())
+        
+        val floatingActionButton = onView(
+allOf(withId(R.id.fab1), withContentDescription("Agregar Album"),
+childAtPosition(
+childAtPosition(
+withId(R.id.nav_host_fragment),
+0),
+1),
+isDisplayed()))
+        floatingActionButton.perform(click())
+        
+        val appCompatEditText = onView(
+allOf(withId(R.id.nameEditText),
+childAtPosition(
+childAtPosition(
+withId(R.id.nav_host_fragment),
+0),
+0),
+isDisplayed()))
+        appCompatEditText.perform(click())
+        
+        val appCompatEditText2 = onView(
+allOf(withId(R.id.nameEditText),
+childAtPosition(
+childAtPosition(
+withId(R.id.nav_host_fragment),
+0),
+0),
+isDisplayed()))
+        appCompatEditText2.perform(replaceText("Test"), closeSoftKeyboard())
+        
+        val appCompatEditText3 = onView(
+allOf(withId(R.id.coverEditText),
+childAtPosition(
+childAtPosition(
+withId(R.id.nav_host_fragment),
+0),
+1),
+isDisplayed()))
+        appCompatEditText3.perform(replaceText("https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg"), closeSoftKeyboard())
+        
+        val appCompatEditText4 = onView(
+allOf(withId(R.id.releaseDateEditText),
+childAtPosition(
+childAtPosition(
+withId(R.id.nav_host_fragment),
+0),
+2),
+isDisplayed()))
+        appCompatEditText4.perform(replaceText("2000-01-01"), closeSoftKeyboard())
+        
+        val appCompatEditText5 = onView(
+allOf(withId(R.id.descriptionEditText),
+childAtPosition(
+childAtPosition(
+withId(R.id.nav_host_fragment),
+0),
+3),
+isDisplayed()))
+        appCompatEditText5.perform(click())
+        
+        val appCompatEditText6 = onView(
+allOf(withId(R.id.descriptionEditText),
+childAtPosition(
+childAtPosition(
+withId(R.id.nav_host_fragment),
+0),
+3),
+isDisplayed()))
+        appCompatEditText6.perform(click())
+        
+        val appCompatEditText7 = onView(
+allOf(withId(R.id.descriptionEditText),
+childAtPosition(
+childAtPosition(
+withId(R.id.nav_host_fragment),
+0),
+3),
+isDisplayed()))
+        appCompatEditText7.perform(replaceText("Buscando América es el primer álbum de la banda de Rubén Blades y Seis del Solar lanzado en 1984. La producción, bajo el sello Elektra, fusiona diferentes ritmos musicales tales como la salsa, reggae, rock, y el jazz latino. El disco fue grabado en Eurosound Studios en Nueva York entre mayo y agosto de 1983."), closeSoftKeyboard())
+        
+        val appCompatEditText8 = onView(
+allOf(withId(R.id.genreEditText),
+childAtPosition(
+childAtPosition(
+withId(R.id.nav_host_fragment),
+0),
+4),
+isDisplayed()))
+        appCompatEditText8.perform(replaceText("Rock"), closeSoftKeyboard())
+        
+        val appCompatEditText9 = onView(
+allOf(withId(R.id.recordLabelEditText),
+childAtPosition(
+childAtPosition(
+withId(R.id.nav_host_fragment),
+0),
+5),
+isDisplayed()))
+        appCompatEditText9.perform(replaceText("Test"), closeSoftKeyboard())
+        
+        val materialButton2 = onView(
+allOf(withId(R.id.submitButton), withText("Guardar"),
+childAtPosition(
+childAtPosition(
+withId(R.id.nav_host_fragment),
+0),
+6),
+isDisplayed()))
+        materialButton2.perform(click())
+        
+        pressBack()
+        
+        val materialButton3 = onView(
+allOf(withId(R.id.BtnColeccionista), withText("Coleccionista"),
+childAtPosition(
+allOf(withId(R.id.container),
+childAtPosition(
+withId(android.R.id.content),
+0)),
+1),
+isDisplayed()))
+        materialButton3.perform(click())
+        
+        val recyclerView = onView(
+allOf(withId(R.id.albumsRv),
+childAtPosition(
+withClassName(`is`("android.widget.FrameLayout")),
+2)))
+        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
+        }
+    
+    private fun childAtPosition(
+            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+    }
